a3a815373e415d3ff6411b667d0a37e9d135d78e
Abdelouahed-SENANE <abdosenane75@gmail.com>
2025-01-05 18:55:26 +0100

    added new features

--- src/app/features/participates/components/participate-start/participate-start.component.ts
+++ src/app/features/participates/components/participate-start/participate-start.component.ts
@@ -1,53 +1,126 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Chapter, Edition, Question } from '../../../../core/models';
-import { ActivatedRoute } from '@angular/router';
-import { ParticipateService } from '../../services/participates.service';
-import {NgForOf} from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
+import {Chapter, Edition, Question} from '../../../../core/models';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ParticipateService} from '../../services/participates.service';
+import {NgForOf, NgIf} from '@angular/common';
+import {data} from 'autoprefixer';
+import {ignoreElements} from 'rxjs';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-participate-start',
-  imports: [
-    NgForOf
-  ],
+  imports: [NgForOf, FormsModule],
   templateUrl: './participate-start.component.html',
   styleUrl: './participate-start.component.css'
 })
 export class ParticipateStartComponent implements OnInit {
-  private readonly route : ActivatedRoute = inject(ActivatedRoute)
-  private readonly service : ParticipateService = inject(ParticipateService)
-  edition! : Edition
-  data : {title : string , questions : Question[]}[] = []
-  chapterIndex : number = 0
-  questionIndex : number = 0
-  ngOnInit(): void {
-    this.loadEdition()
+  private readonly route: ActivatedRoute = inject(ActivatedRoute)
+  private readonly router: Router = inject(Router)
+  private readonly service: ParticipateService = inject(ParticipateService)
+  edition!: Edition
+  data: { title: string, questions: Question[] }[] = []
+  chapterIndex: number = 0
+  questionIndex: number = 0
+  isFinished: boolean = false;
+  isLoading: boolean = true;
+  multi: { questionId: string, answers: {answerId : string}[] } = {
+    questionId: '',
+    answers: []
+  }
+  single: { questionId: string, answerId: string } = {
+    questionId: '',
+    answerId: ''
   }
 
-  loadEdition() : void {
+  async ngOnInit(): Promise<void> {
+
+    try {
+      await this.loadEdition();
+      this.loadProgress();
+      setTimeout(() => {
+        this.isLoading = false;
+      } , 3000)
+    } catch (error) {
+      console.error('Error loading edition:', error);
+    }
+
+  }
+
+  loadProgress(): void {
+    const progress = localStorage.getItem('progress');
+    console.log(progress);
+    if (progress) {
+      const parse = JSON.parse(progress);
+      if (this.edition.id == parse.editionId) {
+        this.questionIndex = parse.questionIndex
+        this.chapterIndex = parse.chapterIndex
+      }
+
+    }
+  }
+
+  loadEdition(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
       this.route.paramMap.subscribe(params => {
         const id = params.get('id')
         if (id) {
           this.service.readEdition(id).subscribe({
-            next : (res) =>{
-              this.edition= res.data.edition
+            next: (res) => {
+              this.edition = res.data.edition
               if (this.edition.chapters) {
                 this.findChapterAndHisQuestion(this.edition.chapters)
               }
               console.log(this.data)
+              resolve();
             },
-            error : (err) => {
+            error: (err) => {
               console.error(err)
+              reject(err)
             }
           })
+        } else {
+          reject("No ID found in parameters")
         }
       })
-    }
+    })
 
+  }
+
+  onChangeAnswers(answerId: string, event: Event): void {
+
+    const checkbox = event.target as HTMLInputElement;
+    this.multi.questionId = this.data[this.chapterIndex].questions[this.questionIndex].id
+    if (checkbox.checked) {
+      if (!this.multi.answers.some(a => a.answerId === answerId)) {
+        this.multi.answers.push({answerId : answerId})
+      }
+    } else {
+      this.multi.answers = this.multi.answers.filter(a => a.answerId != answerId)
+    }
+    console.log(this.multi)
+  }
+
+  onChangeAnswer(answerId: string): void {
+    this.single.questionId = this.data[this.chapterIndex].questions[this.questionIndex].id
+    this.single.answerId = answerId
+    console.log(this.single.questionId)
+  }
+
+  submitForm(newParticipation: any) {
+    this.service.addParticipation(newParticipation ,this.edition.survey.id).subscribe({
+      next: (res) => {
+        console.log(res)
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
+  }
 
   private findChapterAndHisQuestion(chapters: Chapter[]) {
     for (const chapter of chapters) {
       if (chapter.questions && chapter.questions.length > 0) {
-        this.data.push({ title: chapter.title, questions: chapter.questions });
+        this.data.push({title: chapter.title, questions: chapter.questions});
       } else {
         if (chapter.subchapters && chapter.subchapters.length > 0) {
           this.findChapterAndHisQuestion(chapter.subchapters);
@@ -56,6 +129,65 @@ export class ParticipateStartComponent implements OnInit {
     }
   }
 
+  next() {
 
-  protected readonly inject = inject;
+    const currentQuestion: Question = this.data[this.chapterIndex].questions[this.questionIndex]
+
+    if (currentQuestion.type == "SINGLE_CHOICE") {
+      this.submitForm(this.single)
+    } else {
+      this.submitForm(this.multi)
+    }
+
+    const currentChapter = this.data[this.chapterIndex];
+
+    if (!currentChapter || !currentChapter.questions) {
+      console.error('Chapter not found.');
+      return;
+    }
+
+    const TOTAL_QUESTIONS = currentChapter.questions.length;
+
+    if (this.questionIndex < TOTAL_QUESTIONS - 1) {
+      this.questionIndex++;
+    } else if (this.chapterIndex < this.data.length - 1) {
+      this.questionIndex = 0;
+      this.chapterIndex++;
+    } else {
+      this.isFinished = true;
+      setTimeout(() => {
+        this.router.navigate(['/participate']);
+      },5000)
+      this.restProgress()
+      return;
+    }
+    this.saveProgress()
+  }
+
+
+  private saveProgress() {
+    const progress = {editionId: this.edition.id, chapterIndex: this.chapterIndex, questionIndex: this.questionIndex}
+    localStorage.setItem("progress", JSON.stringify(progress));
+  }
+
+  private restProgress() {
+    const progress = {editionId: this.edition.id, chapterIndex: 0, questionIndex: 0}
+
+    localStorage.setItem("progress", JSON.stringify(progress));
+  }
+
+
+  skip() {
+
+    if (this.chapterIndex < this.data.length - 1) {
+      this.chapterIndex++;
+      this.questionIndex = 0;
+      console.log(`Skipped to Chapter Index: ${this.chapterIndex}, Question Index: ${this.questionIndex}`);
+    } else {
+
+      console.log("No more chapters to skip to.");
+
+    }
+  }
+
 }
--- src/app/features/participates/services/participates.service.ts
+++ src/app/features/participates/services/participates.service.ts
@@ -1,11 +1,15 @@
 import { inject, Injectable, OnInit } from "@angular/core";
 import { SurveyService } from "../../surveys/services/surveys.service";
 import { EditionService } from "../../editions/services/editions.service";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {env} from '../../../../env/env';
 
 @Injectable({
       providedIn:"root"
 })
 export class ParticipateService {
+      private readonly http: HttpClient = inject(HttpClient);
       private readonly editionService : EditionService = inject(EditionService);
 
       public readAllEditions() {
@@ -13,6 +17,10 @@ export class ParticipateService {
       }
       public readEdition(id : string) {
             return this.editionService.getEdition(id)
+      }
+
+      public addParticipation(newParticipation : any , surveyId : string)  : Observable<any> {
+        return this.http.post<any>(env.API_URL + "/surveys/"+ surveyId+"/participate", newParticipation)
       }
 
 }
